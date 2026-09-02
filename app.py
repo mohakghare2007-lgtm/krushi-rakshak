3fd2aa97f27e9ef99820f448d89dfa388cffc6fe
mohakghare2007-lgtm <mohakghare2007@gmail.com>
2026-09-02 12:22:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -410,6 +410,7 @@ def feedback():
 # 6. ENTRY POINT
 # ---------------------------------------------------------------------------
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     app.run(debug=True, host="0.0.0.0", port=5000)
